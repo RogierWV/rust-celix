83e24b0ddca9aa7cd0f0f1fb735cb7b323ff8ccb
Rogier Velting <rogiervelting@gmail.com>
2016-03-14 10:57:41 +0100

    Now copies base bundles into deploy dir

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 extern crate rustc_serialize;
 extern crate toml;
-use std::env::args;
+// use std::env::args;
 use std::fs::{File,metadata,copy};
+// use std::path::Path;
 use std::io::prelude::*;
 use std::process::Command;
 use rustc_serialize::json::Json;
 
 fn main() {
+	println!("cargo-celix version {}", env!("CARGO_PKG_VERSION"));
 	if !check_built(".so") {
 		println!("Need to build!");
 		println!("{}",cargo("build"));
 	}
-	bundle();
+	copy_bundles("/usr/local/share/celix");
+	create_bundle();
+	write_config();
 }
 
 fn manifest() -> String {
@@ -26,14 +30,26 @@ Private-Library: lib{}.so
 ", package_name,package_name,toml_lookup("package.version"),package_name,package_name);
 }
 
-fn bundle() {
+fn copy_bundles(celix_dir: &str) {
+	let bundle_dir = celix_dir.to_string() + "/bundles/";
+	let _ = Command::new("mkdir").arg("-p").arg("deploy/bundles").output().unwrap();
+	copy(bundle_dir.as_str().clone().to_string() + "shell.zip", "deploy/bundles/shell.zip").unwrap();
+	copy(bundle_dir.as_str().clone().to_string() + "shell_tui.zip", "deploy/bundles/shell_tui.zip").unwrap();
+}
+
+fn write_config(){
+	let conf = format!("cosgi.auto.start.1=bundles/{}.zip bundles/shell_tui.zip bundles/shell.zip", toml_lookup("package.name").replace("-","_"));
+	File::create("deploy/config.properties").unwrap().write_all(conf.as_bytes()).unwrap();
+}
+
+fn create_bundle() {
 	let tmpdir : String = String::from_utf8_lossy(&Command::new("mktemp").arg("-dt").arg("rust-celix.XXXXXXXXXXXXXX").output().unwrap().stdout).into_owned().trim_right().to_string() + "/";
-	copy(get_lib_name("target/release/lib",".so"),get_lib_name((tmpdir.as_str().clone().to_string()+"lib").as_str().clone(),".so"));
+	let _ = copy(get_lib_name("target/release/lib",".so"),get_lib_name((tmpdir.as_str().clone().to_string()+"lib").as_str().clone(),".so"));
 	let _ = Command::new("mkdir").arg("-p").arg(tmpdir.as_str().clone().to_string()+"META-INF").output().unwrap().stdout;
 	File::create(tmpdir.as_str().clone().to_string()+"META-INF/MANIFEST.MF").unwrap().write_all(manifest().as_bytes()).unwrap();
 	println!("{}", String::from_utf8_lossy(&Command::new("ls").current_dir(tmpdir.as_str().clone()).output().unwrap().stdout));
 	println!("{}", String::from_utf8_lossy(&Command::new("zip").current_dir(tmpdir.as_str().clone()).arg(get_lib_name("",".zip")).arg(get_lib_name("lib",".so")).arg("META-INF/MANIFEST.MF").output().unwrap().stdout));
-	copy(get_lib_name(tmpdir.as_str().clone(),".zip"),("deploy/bundles/".to_string()+toml_lookup("package.name").replace("-","_").as_str()).to_string()+".zip");
+	let _ = copy(get_lib_name(tmpdir.as_str().clone(),".zip"),("deploy/bundles/".to_string()+toml_lookup("package.name").replace("-","_").as_str()).to_string()+".zip");
 	let _ = Command::new("rm").arg("-rf").arg(tmpdir).output().unwrap().stdout;
 }
 
