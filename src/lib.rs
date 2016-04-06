45c013679df4dba99abebd6dfa77b66700f1ff3d
Rogier Velting <rogiervelting@gmail.com>
2016-04-06 13:36:44 +0200

    Added constant to temporarily fix missing values

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code, bad_style)]
 
 pub const ENOMEM: i32 = 12;
+pub const celix_thread_default : (i32,i32) = (0,0);
 
 include!(concat!(env!("OUT_DIR"), "/celix_bind.rs"));
 include!(concat!(env!("OUT_DIR"), "/constants.rs"));
+
+// macros for functions needed by celix
