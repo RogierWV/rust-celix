16ff89417375d3f2c2a063fc7ab3ffd808d8c0ea
Rogier Velting <rogiervelting@gmail.com>
2016-04-06 14:06:31 +0200

    Fixed const value

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #![allow(dead_code, bad_style)]
 
 pub const ENOMEM: i32 = 12;
-pub const celix_thread_default : (i32,i32) = (0,0);
+pub const celix_thread_default : celix_thread_t = celix_thread_t {
+    threadInitialized: 0,
+    thread: 0,
+};
 
 include!(concat!(env!("OUT_DIR"), "/celix_bind.rs"));
 include!(concat!(env!("OUT_DIR"), "/constants.rs"));
 
-// macros for functions needed by celix
+// TODO: macros for functions needed by celix
